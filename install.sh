5b220e30ea2e831a2dcd37a7c0565b203e8fa9bf
Yuta Katayama <uosotm@gmail.com>
2018-03-04 23:17:29 +0900

    Add directory creation for color themes

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+pushd $(dirname $0)
+
 for f in .??*
 do
   if [ "$f" = ".git" ]; then continue; fi
@@ -9,8 +11,13 @@ do
   ln -sfnv $PWD/$f $HOME/$f
 done
 
+if [ ! -d  ~/.vim/colors ]; then
+  mkdir -p ~/.vim/colors
+fi
+
 # Install vim-plug
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+popd
 exit 0
